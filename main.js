d588c6ced8ad97432499d00afad1bb8ecda52afd
Amir Eldor <amir@eize.ninja>
2015-07-19 22:24:52 +0300

    Playing around with jade and express

---
+++ main.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+    res.render('base');
+});
+
+app.get('/:proj', function (req, res) {
+    var proj = req.params.proj || '';
+    res.render('article', { title: 'HI!', message: 'kubia2' + proj });
+});
+
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Listening at %s:%s", host, port);
+});
+
+/*
+exports.init = function() {
+    console.log("hi!");
+}
+*/
