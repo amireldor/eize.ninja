68751139880aee2b1ceafaecd39c38332ef4f9f9
Amir Eldor <amir@eize.ninja>
2015-09-19 16:36:12 +0300

    'config' from JSON to regular JS (for expressions)

---
+++ config.js
@@ -0,0 +1,6 @@
+var host = "localhost";
+module.exports = {
+    "host": host,
+    "port": 3000,
+    "root_url": "http://"+host+":3000"
+}
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var routes = require('./routes');
-var config = require('./config.json');
+var config = require('./config');
 
 var app = express();
 
